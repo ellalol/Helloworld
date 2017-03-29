ace0db1133f6437c064555a9bd40383c304a5290
Min Luo <minluoau@gmail.com>
2017-03-29 23:40:08 +1100

    Test in pycharm successfully

--- Lists.py
+++ Lists.py
@@ -4,23 +4,22 @@
 if __name__ == "__main__":
     arr = list();
     # N = int(raw_input())
-    N = "12\ninsert 0 5\ninsert 1 10"
+    N = "12\ninsert 0 5\ninsert 1 10\ninsert 0 6\nprint\nremove 6\nappend 9\nappend 1\nsort\nprint\npop\nreverse\nprint"
     commands = N.split("\n")
     for n in range(1,len(commands)):
         str = commands[n]
         list = str.split(" ")
         if list[0] == "insert":
             arr.insert(int(list[1]),int(list[2]))
-            print(arr)
         elif list[0] == "print":
             print(arr)
         elif list[0] == "remove":
-            arr = arr.remove(int(list[1]))
+            arr.remove(int(list[1]))
         elif list[0] == "append":
-            arr = arr.append(int(list[1]))
+            arr.append(int(list[1]))
         elif list[0] == "sort":
-            arr = arr.sort()
+            arr.sort()
         elif list[0] == "pop":
-            arr = arr.pop()
+            arr.pop()
         elif list[0] == "reverse":
-            arr = arr.reverse()
+            arr.reverse()
