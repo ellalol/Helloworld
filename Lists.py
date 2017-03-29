039ca74db1186aa0c563c3e907e2b8f9dc330e17
Min Luo <minluoau@gmail.com>
2017-03-29 23:29:49 +1100

    This is a Lists.py

--- Lists.py
+++ Lists.py
@@ -1,11 +1,26 @@
+#https://www.hackerrank.com/challenges/python-lists
+#The script can run in pycharm, but not in website
+# to test in website, change the N = int(raw_input())
 if __name__ == "__main__":
     arr = list();
-    N = "12\ninsert 5 7\ninsert 0 6"
+    # N = int(raw_input())
+    N = "12\ninsert 0 5\ninsert 1 10"
     commands = N.split("\n")
     for n in range(1,len(commands)):
-        print(commands[n])
         str = commands[n]
         list = str.split(" ")
-        print(list)
-        if list[0] == 'insert':
-            arr.insert(list[1],list[2])
+        if list[0] == "insert":
+            arr.insert(int(list[1]),int(list[2]))
+            print(arr)
+        elif list[0] == "print":
+            print(arr)
+        elif list[0] == "remove":
+            arr = arr.remove(int(list[1]))
+        elif list[0] == "append":
+            arr = arr.append(int(list[1]))
+        elif list[0] == "sort":
+            arr = arr.sort()
+        elif list[0] == "pop":
+            arr = arr.pop()
+        elif list[0] == "reverse":
+            arr = arr.reverse()
