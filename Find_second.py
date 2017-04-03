3d51ab12864b8de195f3d932f6100503445f5f61
Min Luo <minluoau@gmail.com>
2017-04-03 17:41:56 +1000

    Done.

--- Find_second.py
+++ Find_second.py
@@ -1,10 +1,16 @@
 # https://www.hackerrank.com/challenges/find-second-maximum-number-in-a-list
 if __name__ == '__main__':
     n = int(input())
-    arr_list = list(map(int, input().split()))
-    my_set = set(arr_list)
-    new_list = list(my_set)
-    new_list.sort(reverse = True)
-    second = new_list.pop(1)
-
+    str = input()
+    str_list = str.split(" ")
+    arr_list = list(map(int, str_list))
+    print(arr_list)
+    arr_list.sort()
+    print(arr_list)
+    MAX = max(arr_list)
+    print(MAX)
+    N = arr_list.count(MAX)
+    print(N)
+    print(len(arr_list))
+    second = arr_list.pop(len(arr_list) - N - 1)
     print(second)
