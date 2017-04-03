2854a50dd6913d1d8737e695e39c5015236c146f
Min Luo <minluoau@gmail.com>
2017-04-03 16:16:40 +1000

    Why sort() does not work?

---
+++ Find_second.py
@@ -0,0 +1,12 @@
+if __name__ == '__main__':
+    n = int(input())
+    arr_list = list(map(int, input().split()))
+    print(arr_list)
+    sort_list = arr_list.sort()
+    print(sort_list) ########## why is None????
+    a = max(arr_list)
+    print(a)
+    N = arr_list.count(a)
+    print(N)
+    second = sort_list.pop(N + 1)
+    print(second)
