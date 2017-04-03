e1a2c8ff0a442cc0a65235df27580d2a2d3153fa
Ethan Yang <ethan.y@outlook.com>
2017-04-03 12:17:04 +1000

    fix tuple.py
    Using raw_input to take inputs.

--- tuples.py
+++ tuples.py
@@ -1,9 +1,8 @@
 # https://www.hackerrank.com/challenges/python-tuples
 import hashlib
 if __name__ == '__main__':
-    n = input() #input "2"
-    t_str = input() #input "2 1"
-    list = t_str.split(" ")
-    print(list)
-    tup = tuple(list)
+    n = int(raw_input())
+    integer_list = map(int, raw_input().split(" "))
+    tup = tuple(integer_list)
     print(hash(tup))
+    
