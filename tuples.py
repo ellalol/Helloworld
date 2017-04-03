108f727befb707cef6dbf64c97639bb6a106d602
Min Luo <minluoau@gmail.com>
2017-04-03 13:21:20 +1000

    A question about the map. See it in code.

--- tuples.py
+++ tuples.py
@@ -1,8 +1,9 @@
 # https://www.hackerrank.com/challenges/python-tuples
 import hashlib
 if __name__ == '__main__':
-    n = int(raw_input())
-    integer_list = map(int, raw_input().split(" "))
-    tup = tuple(integer_list)
+    n = int(input())
+    list = input().split(" ")
+    int_list = map(int, list)
+    print(int_list,type(int_list)) ############# how can I print the new list
+    tup = tuple(int_list)
     print(hash(tup))
-    
