f4269c8b073a3f2863c2c3ab5415b668ad41ffd6
Min Luo <minluoau@gmail.com>
2017-04-05 22:44:47 +1000

    Done. Could you think about a better way to solve this problem?

---
+++ Nested_lists.py
@@ -0,0 +1,30 @@
+# https://www.hackerrank.com/challenges/nested-list
+def getKey(item):
+    return item[1]
+
+if __name__ == '__main__':
+    n = int(input())
+    result_list = list()
+    for i in range(0,n):
+        name = input()
+        score = float(input())
+        result_list.insert(i, [name, score])
+    sort_list = sorted(result_list,key=getKey)
+    MIN = min(sort_list,key=getKey)
+    Min_count = 0
+
+    for i in range(0,n):
+        if sort_list[i][1] == MIN[1]:
+            Min_count += 1
+
+    result = []
+    for j in range(Min_count, n):
+        NEW_MIN = sort_list[Min_count][1]
+        if sort_list[j][1] == NEW_MIN:
+            result.append(sort_list[j][0])
+
+    result.sort()
+    for k in result:
+        print(k)
+
+
